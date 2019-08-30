c25a34515e0bd737d7c98de497d542f76c48b919
Frankie Ye <frankie9793@gmail.com>
2019-08-31 02:34:24 +0800

    Bubble sort

---
+++ Sorting/bubbleSort.py
@@ -0,0 +1,12 @@
+# Time O(N) | # Space O(1)
+def bubbleSort(array):
+    isSorted = False
+    counter = 0
+    while not isSorted:
+        isSorted = True
+        for i in range(len(array) - 1 - counter):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                isSorted = False
+        counter += 1
+    return array
